997ac3488d89efac15828c93d8e2a1f34cb9ef01
ilaha2002 <ilaha_simmi_i@inbox.ru>
2022-11-14 14:01:56 +0400

    algoritmicsTI2

---
+++ scriptTI2.js
@@ -0,0 +1,33 @@
+class Message {
+    constructor(author, time, text) {
+      this.author = author;
+      this.time = time;
+      this.text = text;
+
+      // const time = new Time();
+        // this.time
+    }
+  }
+
+  class Messenger {
+    constructor(name, time, text) {
+        this.history
+
+         
+  } 
+}
+
+const toHtml = {
+    author: 'Eric Cartman',
+    time: '23:25',
+    text: 'good luck',
+  }
+
+
+//   <p>[время] [автор]: [текст]</p>
+  
+  
+    const person2 = new Person ('O');
+    const person3 = new Person ('B');
+
+  console.log(person1, person2);
